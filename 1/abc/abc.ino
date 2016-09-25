fa5ac1ac9321669b29fb9b18e1d8298f6e5ebc6c
vanstorm9 <antlowhur@yahoo.com>
2016-09-24 20:20:09 -0400

    Adding stuff

---
+++ 1/abc/abc.ino
@@ -0,0 +1,23 @@
+#include <Stepper.h>
+const int stepsPerRevolution = 200; 
+Stepper myStepper(stepsPerRevolution, 12,13);    
+const int dirA = 12;
+const int dirB = 13;
+void setup() {
+pinMode(3, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(8, OUTPUT);
+digitalWrite(3, HIGH);
+digitalWrite(11, HIGH);
+digitalWrite(9, LOW);
+digitalWrite(8, LOW);
+myStepper.setSpeed(80);
+}
+
+void loop() {
+ myStepper.step(500);
+ delay(1000);
+ myStepper.step(-500);
+ delay(1000);
+}
